1eede263a40b2d7c10d12bbd982db248b2033b67
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2024-08-03 17:40:03 +0530

    Add project files.

---
+++ EX_TagHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EX_TagHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
